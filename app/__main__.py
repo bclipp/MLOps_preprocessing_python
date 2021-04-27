9d5f5154afcc0cf52727661d3a2a3b799088153e
blipp <6Fqshfzlx3hpKgZEWZbu>
2021-04-27 14:55:07 -0400

    updating

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,21 @@
+from delta.tables import *
+import yfinance as yf
+import time
+import dbutils
+import uuid
+
 
 def main():
-    print("hello world")
+    spark = ...
+    spark.conf.set("spark.sql.execution.arrow.enabled", True)
+    pdf = yf.download(tickers='UBER', period='10y', interval='1d')
+    pdf["AdjClose"] = pdf["Adj Close"]
+    pdf = pdf.drop("Adj Close", axis=1)
+    id = str(uuid.uuid1()).replace('-', '')
+    timestamp = int(time.time())
+    dbutils.fs.mkdirs("/dbfs/datalake")
+    df = spark.createDataFrame(pdf)
+    df.write.format("delta").save(f"/dbfs/datalake/stocks_{id}_{timestamp}/data")
 
 
 if __name__ == "__main__":
